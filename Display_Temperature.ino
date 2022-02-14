835c5ae41a10de0e7a7cc082605f5cdbd5cfdb99
Jordan Gelling <68049810+CurlzG@users.noreply.github.com>
2022-02-14 23:17:05 +1300

    Add files via upload

---
+++ Display_Temperature.ino
@@ -0,0 +1,38 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+#define ONE_WIRE_BUS 5
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("OLED FeatherWing test");
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+
+  Serial.println("OLED begun");
+
+  display.display();
+  delay(1000);
+sensors.begin();
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+  // text display tests
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.display(); // actually display all of the above
+}
+
+void loop() {
+  delay(10000);
+  sensors.requestTemperatures(); 
+   display.println(sensors.getTempCByIndex(0));
+  yield();
+  display.display();
+}
